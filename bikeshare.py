db60e8d356f69f18b6643db3e18703e6976170b5
will zhang <zhanliam21@gmail.com>
2020-06-23 00:04:55 -0400

    Change what consol will ask for user's input (y or n).

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    
+
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     # TO DO: get user input for month (all, january, february, ... , june)
@@ -213,7 +213,7 @@ def main():
         user_stats(df, city)
         rawData(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? (yes or no).\n')
         if restart.lower() != 'yes':
             break
 
